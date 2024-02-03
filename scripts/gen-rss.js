d0799fbf156ca6eb3516ba24a42fc33cd4b9982c
gravelrodnova <16125438+gravelrodnova@users.noreply.github.com>
2024-02-02 21:37:35 -0500

    updating site

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'nova neace',
+    site_url: 'https://gravelrodnova.github.io',
+    feed_url: 'https://gravelrodnova.github.io/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
