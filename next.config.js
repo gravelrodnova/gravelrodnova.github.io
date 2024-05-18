eee742dbe0683f123da6db401fd3f30363201f25
gravelrodnova <16125438+gravelrodnova@users.noreply.github.com>
2024-05-18 16:45:37 -0400

    gay sex

--- next.config.js
+++ next.config.js
@@ -5,3 +5,20 @@ module.exports = {
     unoptimized: true,
   },
 }
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
