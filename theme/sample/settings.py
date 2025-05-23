77e16690639e9adf4eaa58c00bfc7ea62945b552
gravelrodnova <16125438+gravelrodnova@users.noreply.github.com>
2025-05-23 12:05:10 -0400

    gaming

--- theme/sample/settings.py
+++ theme/sample/settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'Tyler Carr'
+AUTHOR = 'sayo'
 SITENAME = 'Blue Penguin Dark'
 SITEURL = 'http://localhost:8000'
 SITELOGO = 'https://www.gravatar.com/avatar/de3ba847c89c8cab9dd7080abebf9df1'
